5677391756960e090b965e89cb0667ba2a270c84
Le Cong Danh <lcdanh996@gmail.com>
2022-09-22 21:57:49 +0700

    adding datase and migration success

--- main.go
+++ main.go
@@ -1,26 +1,38 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/danh996/go-destiny/book"
+	"github.com/danh996/go-destiny/database"
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-func helloWorld(c *fiber.Ctx) {
-	c.Send("Hello, World!")
-}
-
 func setupRoutes(app *fiber.App) {
-	app.Get("/", helloWorld)
-
 	app.Get("/api/v1/book", book.GetBooks)
 	app.Get("/api/v1/book/:id", book.GetBook)
 	app.Post("/api/v1/book", book.NewBook)
 	app.Delete("/api/v1/book/:id", book.DeleteBook)
 }
 
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("Database Migrated")
+}
 func main() {
 	app := fiber.New()
+	initDatabase()
 
 	setupRoutes(app)
 	app.Listen(3000)
+
+	defer database.DBConn.Close()
 }
